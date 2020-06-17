105ec7ed928befa4442fe3c138d5469e454e948c
rwedzony <rwedzony@gmail.com>
2020-06-17 16:07:46 +0200

    Initial commit aftter generating files using Spring Boot initializer.

---
+++ src/test/java/com/rafwedz/employee/EmployeeManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafwedz.employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
