ccbe34a9fd8c2284b32158da60901e270bcbca3e
rwedzony <rwedzony@gmail.com>
2020-06-19 12:19:43 +0200

    Added Employee Controller, sql Script to create PostgreSql database and index html for basic view

---
+++ src/main/resources/sql_scripts/ems_database_creation_script.sql
@@ -0,0 +1,32 @@
+
+
+
+-- create database for system
+-- drop if exist
+--DROP DATABASE ems;
+
+-- CREATE DATABASE ems
+--     WITH
+--     OWNER = postgres
+--     ENCODING = 'UTF8'
+--     LC_COLLATE = 'Polish_Poland.1250'
+--     LC_CTYPE = 'Polish_Poland.1250'
+--     TABLESPACE = pg_default
+--     CONNECTION LIMIT = -1;
+
+-- create table employee for system
+
+DROP TABLE IF EXISTS employee;
+
+CREATE TABLE employee(
+   employee_id INT PRIMARY KEY     NOT NULL,
+   first_name           varchar(255)    NOT NULL,
+   last_name            varchar(255)     NOT NULL,
+   email        varchar(255)     NOT NULL
+);
+
+-- fill employee table with sample data
+
+Insert into employee values ('1', 'Janina','Nowak','janinanowak@wp.pl');
+Insert into employee values ('2', 'Marek','Gladysz','marekgladysz@onet.pl');
+Insert into employee values ('3', 'Dominika','Paradowska','dominikaparadowska@interia.pl');
