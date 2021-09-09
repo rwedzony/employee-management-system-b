d1111e424cd24548ce97338c1c3521ee9fe52de2
rwedzony <rwedzony@gmail.com>
2021-09-09 23:39:41 +0200

    AuthServicer refactor

--- src/main/java/com/rafwedz/employee/login/AuthService.java
+++ src/main/java/com/rafwedz/employee/login/AuthService.java
@@ -14,31 +14,27 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 
-
 @Service
 @AllArgsConstructor
 @Transactional
 public class AuthService {
-
     private final EmployeeRepository employeeRepository;
     private final CustomUserDetailsService customUserDetailsService;
     private final AuthenticationManager authenticationManager;
     private final JwtUtil jwtutil;
 
-
     public ResponseEntity<?> login(LoginRequest loginRequest) throws Exception {
         try {
             Authentication authenticate = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getUsername(),
                     loginRequest.getPassword()));
-        }
-        catch (BadCredentialsException e){
-            throw new Exception("Incorrect username or password",e);
+        } catch (BadCredentialsException e) {
+            throw new Exception("Incorrect username or password", e);
         }
         final UserDetails userDetails = customUserDetailsService
                 .loadUserByUsername(loginRequest.getUsername());
 
         final String jwt = jwtutil.generateToken(userDetails);
-        String loggedUserName=loginRequest.getUsername();
+        String loggedUserName = loginRequest.getUsername();
 
         return ResponseEntity.ok(new AuthenticationResponse(
                 employeeRepository.findIdByUsername(loggedUserName),
