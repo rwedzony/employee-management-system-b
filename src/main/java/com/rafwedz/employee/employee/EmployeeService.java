aacc2af163b8f6300498674bc577c6a2c323f467
rwedzony <rwedzony@gmail.com>
2021-09-09 23:56:00 +0200

    Employee Service refactor

--- src/main/java/com/rafwedz/employee/employee/EmployeeService.java
+++ src/main/java/com/rafwedz/employee/employee/EmployeeService.java
@@ -1,8 +1,6 @@
 package com.rafwedz.employee.employee;
 
 
-import com.rafwedz.employee.employee.Employee;
-import com.rafwedz.employee.employee.EmployeeRepository;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +9,7 @@ import java.util.Optional;
 
 @Service
 public class EmployeeService {
+
     private final EmployeeRepository employeeRepository;
 
     public EmployeeService(EmployeeRepository employeeRepository) {
@@ -21,27 +20,31 @@ public class EmployeeService {
         return employeeRepository.findAll(Sort.by(Sort.Direction.ASC, "id"));
     }
 
-    public long count(){ return employeeRepository.count();
+    public long count() {
+        return employeeRepository.count();
     }
 
-    public void save(final Employee employee){
+    public void save(final Employee employee) {
         employeeRepository.save(employee);
     }
 
-    public Optional<Employee> getEmployeeById(Long emp_id){
-       return employeeRepository.findById(emp_id);
+    public Optional<Employee> getEmployeeById(Long emp_id) {
+        return employeeRepository.findById(emp_id);
     }
 
-    public void deleteEmployeeById(Long emp_id){
-       employeeRepository.deleteById(emp_id);
+    public void deleteEmployeeById(Long emp_id) {
+        employeeRepository.deleteById(emp_id);
     }
 
-    public void delete(Employee employee){
+    public void delete(Employee employee) {
         employeeRepository.delete(employee);
     }
 
-    public int getAllEmployeesCount(){return employeeRepository.getEmployeeCount(); }
+    public int getAllEmployeesCount() {
+        return employeeRepository.getEmployeeCount();
+    }
 
-    public int getAllEmployeesWagesCount(){return employeeRepository.getWagesCount(); }
-
+    public int getAllEmployeesWagesCount() {
+        return employeeRepository.getWagesCount();
+    }
 }
