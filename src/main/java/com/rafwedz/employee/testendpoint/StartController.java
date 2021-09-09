afdd7848ec89c939b47ca825e20005689e4c552e
rwedzony <rwedzony@gmail.com>
2021-09-09 23:50:10 +0200

    Start Cantroller refactor

--- src/main/java/com/rafwedz/employee/testendpoint/StartController.java
+++ src/main/java/com/rafwedz/employee/testendpoint/StartController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @CrossOrigin
 public class StartController {
+
     @GetMapping("/start")
     public String redirectToEmployees(){
         StringBuilder sb=new StringBuilder();
